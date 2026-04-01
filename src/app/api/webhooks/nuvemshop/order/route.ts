c1213b20e5325a0c6fc339810f37bcf7ab6ae502
jhanryw <wjhanry@gmail.com>
2026-04-01 00:01:35 -0300

    123

--- src/app/api/webhooks/nuvemshop/order/route.ts
+++ src/app/api/webhooks/nuvemshop/order/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export async function POST(request: Request) {
   const raw = await request.text()
 
-  console.log('🔥 WEBHOOK NUVEMSHOP RECEBIDO')
+  console.log('🔥 WEBHOOK NUVEMSHOP RECEBIDO!!')
   console.log(raw)
 
   return NextResponse.json({ ok: true })
